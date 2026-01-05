b24e6fcc3dd90bb20c86d3944644902a67268c10
Divyanshu <dgsingh1937@gmail.com>
2026-01-05 10:23:08 +0530

    Created Tracking page percentage of products delivery time

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,13 +1,44 @@
-import { Link } from 'react-router';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { Link, useParams } from 'react-router';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import './TrackingPage.css'
 
-export function TrackingPage() {
+export function TrackingPage({ cart }) {
+    const { orderId, productId } = useParams();
+    const [order, setOrder] = useState(null);
+
+    useEffect(() => {
+        const fetchTrackingData = async () => {
+            const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
+        };
+
+        fetchTrackingData();
+    }, [orderId]);
+
+    if (!order) {
+        return null;
+    }
+
+    const orderProduct = order.products.find((orderProduct) => {
+        return orderProduct.productId === productId;
+    });
+
+    const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+    const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+
+    let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+    if (deliveryPercent > 100) {
+        deliveryPercent = 100;
+    }
+
     return (
         <>
             <title>Tracking</title>
             <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
-            <Header />
+            <Header cart={cart} />
 
             <div className="tracking-page">
                 <div className="order-tracking">
@@ -16,18 +47,19 @@ export function TrackingPage() {
                     </Link>
 
                     <div className="delivery-date">
-                        Arriving on Monday, June 13
+                        {deliveryPercent >= 100 ? 'Delivered on ' : 'Arriving on '}
+                        {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
                     </div>
 
                     <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                        {orderProduct.product.name}
                     </div>
 
                     <div className="product-info">
-                        Quantity: 1
+                        Quantity: {orderProduct.quantity}
                     </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                    <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
                         <div className="progress-label">
@@ -42,7 +74,9 @@ export function TrackingPage() {
                     </div>
 
                     <div className="progress-bar-container">
-                        <div className="progress-bar"></div>
+                        <div className="progress-bar"
+                            style={{ width: `${deliveryPercent}%` }}
+                        ></div>
                     </div>
                 </div>
             </div>
