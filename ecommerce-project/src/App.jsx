87e51bb4e40a0dd0cb164367551724775e78aeb1
Divyanshu <dgsingh1937@gmail.com>
2026-01-05 11:15:38 +0530

    Create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -17,23 +17,22 @@ function App() {
   //       setCart(response.data);
   //     });
   // }, []);
-
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  }
   useEffect(() => {
-    const fetchAppData = async () => {
-      const response = await axios.get('/api/cart-items?expand=product');
-      setCart(response.data);
-    }
-    fetchAppData();
+    loadCart();
   }, []);
 
   return (
     <Routes>
       {/* <Route path="/" element={<HomePage />}></Route> */}
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking/:orderId/:productId" element={<TrackingPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="*" element={<NotFoundPage cart={cart} />} />
     </Routes>
   )
 }
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -4,7 +4,7 @@ import { Header } from '../../components/Header';
 import { ProductsGrid } from './Products.Grid';
 import './HomePage.css'
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
     const [products, setProducts] = useState([]);
 
 
@@ -39,7 +39,7 @@ export function HomePage({ cart }) {
 
 
             <div className="home-page">
-                <ProductsGrid products={products} />
+                <ProductsGrid products={products} loadCart={loadCart} />
             </div></>
 
     )
